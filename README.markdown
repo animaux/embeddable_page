698603b13c7151ce792d8d76fb3e5d7d1914dbca
Alexander Rutz <ar@animaux.de>
2017-02-02 12:19:08 +0100

    Init (2nd try)

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# Embeddable Page
+
+Allows you to add a page type `embeddable` to a symphony page in order to allow embedding in an iFrame via `X-Frame-Options —> ALLOWALL`.
+
+## Acknowledgements
+
+Many thanks to [Jonathan Mifsud](https://github.com/jonmifsud) for initial code! I also butchered the `maintenance_mode` extension in order to built this.
+
+## Installation & Updating
+
+Information about [installing and updating extensions](http://getsymphony.com/learn/tasks/view/install-an-extension/) can be found in the Symphony documentation at <http://getsymphony.com/learn/>.
---
+++ extension.driver.php
@@ -0,0 +1,72 @@
+<?php
+
+Class extension_embeddable_page extends Extension
+{
+
+    public function getSubscribedDelegates()
+    {
+        return array(
+
+            array('page' => '/frontend/',
+                  'delegate' => 'FrontendPreRenderHeaders',
+                  'callback' => 'frontendPreRenderHeaders',
+            ),
+
+            array('page' => '/blueprints/pages/',
+                  'delegate' => 'AppendPageContent',
+                  'callback' => '__appendType',
+            )
+            
+        );
+    }
+
+    /**
+     * allow to be placed in an iframe
+     */
+    public function frontendPreRenderHeaders($context) {
+        $frontendPage = Frontend::instance()->Page();
+        $pageTypes = $frontendPage->Params()['page-types'];
+        
+        if (in_array('embeddable', $pageTypes)) {
+            $frontendPage->removeHeaderFromPage('X-Frame-Options');
+            $frontendPage->removeHeaderFromPage('Access-Control-Allow-Origin');
+            $frontendPage->addHeaderToPage('X-Frame-Options','ALLOWALL');
+        }
+
+    }
+
+    /**
+     * Append type for embeddable pages to page editor.
+     *
+     * @param array $context
+     *  delegate context
+     */
+    public function __appendType($context)
+    {
+        // Find page types
+        $elements = $context['form']->getChildren();
+        $fieldset = $elements[0]->getChildren();
+        $group = $fieldset[2]->getChildren();
+        $div = $group[1]->getChildren();
+        $types = $div[2]->getChildren();
+
+        // Search for existing embeddable type
+        $flag = false;
+
+        foreach ($types as $type) {
+
+            if ($type->getValue() === 'embeddable') {
+
+                $flag = true;
+            }
+        }
+
+        // Append embeddable type
+        if ($flag === false) {
+
+            $mode = new XMLElement('li', 'embeddable');
+            $div[2]->appendChild($mode);
+        }
+    }
+
+}
